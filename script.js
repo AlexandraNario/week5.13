e5d05c6239536d8fbb4c91f126c9068215e0561b
Alexandra Nario <alexandranario@gmail.com>
2023-11-05 23:55:16 -0800

    add script.js and index.html

---
+++ script.js
@@ -0,0 +1,19 @@
+//Given the following html code, make the style changes listed in the comments using JavaScript:
+//example 1:
+document.getElementById("example1").style.backgroundColor = "yellow";
+//example 2:
+document.getElementById("example2").style.fontSize = "24px";
+//note for me 'getElementById' is for Id
+//querySelector is for class and need a period select to work
+//example 3:
+document.querySelector(".example3").style.color = "red";
+//example 4:
+document.getElementById("example4").style.border = "1px solid black";
+//example 5:
+document.getElementById("example5").style.backgroundImage = "url('https://www.w3schools.com/js/pic_bulboff.gif')";
+  
+//example 6:
+document.getElementById("example6").style.display = "none"
+
+//example 7:
+document.querySelector(".example7").style.color = "green"
